0dc654c6f738096a43362ae0501942064047d581
PetarPan <71749611+PetarPan@users.noreply.github.com>
2022-02-28 18:20:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,96 +1,115 @@
-/** @format */
-// varijable
+// variables
 
 let vr = document.querySelector("#vr");
 let pm = document.querySelector("#pm");
 let h = document.querySelector("#h");
 let ps = document.querySelector("#ps");
 let ts = document.querySelector("#ts");
-let tr = document.querySelector('#tr');
-let q = document.querySelector('#q');
-let period = document.querySelector('#period');
-let tkomp = document.querySelector('#tkomp')
-let protekla = document.querySelector('#protekla')
-let kvalitet = document.querySelector('#kvalitet')
-let form = document.querySelector('#form')
-//konstante
+let tr = document.querySelector("#tr");
+let q = document.querySelector("#q");
+let period = document.querySelector("#period");
+let tkomp = document.querySelector("#tkomp");
+let protekla = document.querySelector("#protekla");
+let kvalitet = document.querySelector("#kvalitet");
+let form = document.querySelector("#form");
+let inputs = document.querySelectorAll("input");
+let vrErr = document.querySelector(".vrErr");
+let hErr = document.querySelector(".hErr");
+let qErr = document.querySelector(".qErr");
+
+let err1 = document.querySelector(".errVr");
+let err2 = document.querySelector(".errH");
+let err3 = document.querySelector(".errQ");
+
+//constant values
 
 pm = 22;
 ps = 1013.25;
 ts = 288.15;
 
 //funkcija
-
 const svedeneFunk = () => {
+  //variables inside function
 
-    //varijable i konstante
+  let rezultat = document.querySelector("#rezultat");
+  let patm = 1016 - 0.108 * h.value;
+  let kvalitet2 = 33338.35;
+  let proteklaKolicina;
 
-    let rezultat = document.querySelector('#rezultat')
-    let patm = 1016 - 0.108 * h.value
-    let kvalitet2 = 33338.35
-    let proteklaKolicina
-
-
-    //uslovi 
-
-    if (h.value == "e" || h.value =='E' || vr.value == 'e' || vr.value == 'E' ) {
-        rezultat.textContent = "Дoзвољен је унос само нумеричких симбола";
-        rezultat.style.color = "red";
-        protekla.textContent = "";
-        return
-    }
-    if (vr.value == "" || vr.value < 0) {
-        rezultat.textContent = "Унос очитане количине је обавезан и мора да буде већи од 0";
-        rezultat.style.color = "red";
-        protekla.textContent = "";
-        return
-    }
-   
-    if (h.value == "" || vr.value < 0) {
-        rezultat.textContent = "Унос параметра 'h', висине је обавезан и мора да буде већи од 0";
-        rezultat.style.color = "red";
-        protekla.textContent = "";
-        return
-    }
-     if (q.value == "" || q.value < 0) {
-        rezultat.textContent = "Унос параметра квалитет гаса је обавезан и мора да буде већи од 0";
-        rezultat.style.color = "red";
-        protekla.textContent = "";
-        return
-    } 
-     if (q.value < 25000) {
-        rezultat.textContent = "Унесите исправан квалитет";
-        rezultat.style.color = "red";
-        protekla.textContent = "";
-        return
-    }
-   
-
-    if (tkomp.value === 'da') {
-       // protekla.textContent = 'Протекло је ' + Math.round(Number(vr.value) * ((pm + patm) / ps)) + ' m³'
-        proteklaKolicina = Number(vr.value) * ((pm + patm) / ps)
-        protekla.textContent = 'Протекло је ' + Math.round(proteklaKolicina) + ' m³'
-       
-
-    } else {
-        //protekla.textContent = 'Протекло је ' + Math.round(Number(vr.value) * ((pm + patm) / ps) * (ts / period.value)) + ' m³'
-        proteklaKolicina = Number(vr.value) * ((pm + patm) / ps) * (ts / period.value)
-        protekla.textContent = 'Протекло је ' + Math.round(proteklaKolicina) + ' m³'
-        
-    }
-    //rezultat 
-    rezultat.textContent = 'Обрачуната количина износи ' + Math.round(proteklaKolicina * Number(q.value) / kvalitet2) + ' m³'
-    //reset polja
-    vr.value = "";
-    h.value = "";
-    ps.value = "";
-    q.value = "";
+  //validation
+  
+  if (isNaN(vr.value) == true) {
+    err1.innerHTML = "Дозвољен је унос само нумеричких карактера";
+    return;
+  } else {
+      err1.innerHTML = ''
+  }
+  if (isNaN(h.value) == true) {
+    err2.innerHTML = "Дозвољен је унос само нумеричких карактера";
+    return;
+  } else {
+    err2.innerHTML = ''
 }
 
-form.addEventListener('submit', (e) => {
+  if (isNaN(q.value) == true) {
+    err3.innerHTML = "Дозвољен је унос само нумеричких карактера";
+    return;
+  } else {
+    err3.innerHTML = ''
+}
 
-    e.preventDefault()
-    
-    svedeneFunk()
-    
+  if (vr.value == "" || vr.value < 0 || vr.value > 10000) {
+    err1.innerHTML =
+      "Унос очитане количине мора да буде позитиван број и не већи од 10000";
+    return;
+  }
+
+  if (h.value == "" || h.value < 0 || h.value > 1100) {
+    err2.innerHTML =
+      "Унос висине мора да буде позитиван број и не већи од 1100";
+    return;
+  }
+  if (q.value == "" || q.value < 0 || q.value < 25000 || q.value > 42000) {
+    err3.textContent = "Квалитет мора да буде у опсегу 25000 - 42000";
+    return;
+  }
+
+
+  //temperature compensator od not?
+  if (tkomp.value === "da") {
+    // protekla.textContent = 'Протекло је ' + Math.round(Number(vr.value) * ((pm + patm) / ps)) + ' m³'
+    proteklaKolicina = Number(vr.value) * ((pm + patm) / ps);
+    protekla.textContent =
+      "Протекло је " + Math.round(proteklaKolicina) + " m³";
+  } else {
+    //protekla.textContent = 'Протекло је ' + Math.round(Number(vr.value) * ((pm + patm) / ps) * (ts / period.value)) + ' m³'
+    proteklaKolicina =
+      Number(vr.value) * ((pm + patm) / ps) * (ts / period.value);
+    protekla.textContent =
+      "Протекло је " + Math.round(proteklaKolicina) + " m³";
+  }
+
+  //result
+  rezultat.classList.remove('none')
+  rezultat.classList.add('display')
+  rezultat.textContent =
+    "Обрачуната количина износи " +
+    Math.round((proteklaKolicina * Number(q.value)) / kvalitet2) +
+    " m³";
+  err1.innerHTML = "";
+  err2.innerHTML = "";
+  err3.innerHTML = "";
+
+  //reset input fields
+  vr.value = "";
+  h.value = "";
+  q.value = "";
+  
+};
+
+
+//submit form
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  svedeneFunk();
 });
